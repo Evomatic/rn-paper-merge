659a1edb7bbbb8ff8e0dbd6248a6112be5303f29
Evan Trujillo <evantrujillo@Evans-MacBook-Pro.local>
2021-11-12 16:23:18 +0900

    added to wrapper

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -2,5 +2,15 @@
 import React from "react";
 
 export default function UXPinWrapper({ children }) {
+
+  let icons = document.createElement('link');
+    icons.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+    icons.setAttribute('rel', 'stylesheet');
+    document.head.appendChild(icons);
+   
+    let roboto = document.createElement('link');
+    roboto.setAttribute('href', 'https://fonts.googleapis.com/css?family=Roboto:300,400,500');
+    roboto.setAttribute('rel', 'stylesheet');
+    document.head.appendChild(roboto);
   return children;
 }
